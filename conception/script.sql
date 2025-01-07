5092784cc6a95cca03fcfc768ec09097b730c118
hichamoubaha <oubahahicham7@gmail.com>
2025-01-07 15:27:53 +0000

    creat the table articles

--- conception/script.sql
+++ conception/script.sql
@@ -21,3 +21,18 @@ CREATE TABLE categories (
     nom_categorie VARCHAR(50) NOT NULL UNIQUE,
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Table articles
+CREATE TABLE articles (
+    id_article INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    contenu TEXT NOT NULL,
+    id_categorie INT,
+    id_auteur INT,
+    statut ENUM('en attente', 'approuve', 'refuse') DEFAULT 'en attente',
+    photo VARCHAR(255) NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_modification TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_categorie) REFERENCES categories(id_categorie),
+    FOREIGN KEY (id_auteur) REFERENCES utilisateurs(id_utilisateur)
+);
