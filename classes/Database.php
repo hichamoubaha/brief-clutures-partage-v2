af2a3845a0b1dd2d9aae9111dba0da47a2087e83
hichamoubaha <oubahahicham7@gmail.com>
2025-01-07 15:46:58 +0000

    modifier la conexion db to adabter avec ma nouveau base de donnes

--- classes/Database.php
+++ classes/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "art_culture_platform";
+    private $db_name = "culture_platform_v2";
     private $username = "root";
     private $password = "";
     private $conn;
