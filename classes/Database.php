c05d59f771e011a9601f925bda9a01cd7cdf8221
hichamoubaha <oubahahicham7@gmail.com>
2025-01-10 12:02:39 +0000

    add databse class to connect with my database

---
+++ classes/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "culture_platform_v2"; // Updated database name
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
